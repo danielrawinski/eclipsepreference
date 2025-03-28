f8222111f8fcbe29966bb5561a25fcc3deb4309e
danielrawinski <daniel.rawinski@gmail.com>
2025-03-28 23:26:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,7 +146,7 @@ if countryselector == 'Poland':
             st.write('The best conditions according to your preferences are in:')
             st.markdown(display_df.sort_values(by=['Preference index'], axis=0, ascending=False).head().to_markdown(index=False))
             
-            polen = gpd.read_file('https://drive.google.com/file/d/1_-V5wS8NdMFYL66E3vY8ihaUXyMgVZ7l/view?usp=sharing')
+            polen = gpd.read_file('https://drive.google.com/file/d/1_-V5wS8NdMFYL66E3vY8ihaUXyMgVZ7l/.shp')
 
             map_sp_df = pd.concat([polen, conditions_for_choice], axis=1)
 
